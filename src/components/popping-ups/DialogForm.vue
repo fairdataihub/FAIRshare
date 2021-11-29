9310fff863b8bbc7e2d6578edc5bd15ef684a87a
Taiji-Yang <taiji.github@gmail.com>
2021-11-29 15:51:44 -0600

    Update DialogForm.vue

--- src/components/popping-ups/DialogForm.vue
+++ src/components/popping-ups/DialogForm.vue
@@ -1,21 +1,40 @@
 <template>
-  <el-dialog title="Please provide">
-    <template #footer>
-      <el-button type="text" class="button" @click="closeDialogFromParent"
-        >SAVE</el-button
-      >
-    </template>
+  <el-dialog>
+    <div class="dialog-Container">
+      <div class="inputField">
+          <div class="centering-Container fix-Width">
+              <div class="inputBar-Header">User Name</div>
+          </div>
+        <el-input class="inputBar" size="large" v-model="input1" placeholder="User Name" />
+      </div>
+
+      <div class="inputField">
+          <div class="centering-Container fix-Width">
+              <div class="inputBar-Header">Password</div>
+          </div>
+        <el-input class="inputBar" size="large" v-model="input2" placeholder="Password" />
+      </div>
+      <div class="bottom">
+        <el-button type="text" class="button" @click="closeDialogFromParent"
+          >SAVE</el-button
+        >
+      </div>
+    </div>
   </el-dialog>
 </template>
 
 <script>
-// import { reactive, toRefs } from "vue";
-
+import { ref } from 'vue'
 export default {
   props: {
     callback: { type: Function },
   },
-  setup() {},
+  setup() {
+    return {
+      input1: ref(''),
+      input2: ref(''),
+    }
+  },
   methods: {
     closeDialogFromParent() {
       this.callback();
@@ -26,6 +45,42 @@ export default {
 
 <style scoped>
 .el-button--text {
-  font-size: 2vmin;
+  font-size: 1.5vw;
+}
+
+.centering-Container {
+  display: flex;
+  align-items: center;
+  justify-content: right;
+}
+
+.fix-Width{
+    width: 10vw;
+}
+
+.dialog-Container{
+    display: flex;
+    flex-direction: column;
+    gap: 2vh;
+}
+
+.inputField {
+  display: flex;
+  gap: 1vw;
+}
+
+.inputBar-Header{
+    font-size: 1.4vw;
+
+}
+
+.inputBar{
+    width: 30vw;
+}
+
+.bottom{
+    display: flex;
+    align-items: flex-end;
+    justify-content: flex-end;
 }
 </style>
