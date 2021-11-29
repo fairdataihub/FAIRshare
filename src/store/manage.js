f39000c7b44ec32154f13aa067b4a546d5537445
Taiji-Yang <taiji.github@gmail.com>
2021-11-29 14:15:26 -0600

    manage account added

---
+++ src/store/manage.js
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia";
+export const useManage = defineStore({
+  // id is required so that Pinia can connect the store to the devtools
+  id: "manageAccount",
+  state: () => ({
+    statusText: "Disconnected",
+    apiKeys: { "zenodo": "", "github": "" },
+  }),
+  getters: {
+    getStatusText() {
+      return this.statusText;
+    },
+  },
+  actions: {
+    checkApiKey(value) {
+      return this.apiKeys[value];
+    },
+
+    async changeStatus(newStatus) {
+      try {
+        this.statusText = newStatus;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    async addApiKey(key, value) {
+      try {
+        this.apiKeys[key] = value;
+        return "OK";
+      } catch (error) {
+        console.log(error);
+        return "ERROR";
+      }
+    },
+  },
+});
---
+++ src/views/manage/ManageAccount.vue
@@ -0,0 +1,275 @@
+<template>
+  <div class="page-Container">
+    <div class="app-Card">
+      <div class="image-Container">
+        <img
+          src="../../assets/github.jpeg"
+          class="image"
+        />
+      </div>
+      <div class="app-Card-Content">
+        <div class="app-Card-Status">
+          <div class="centering-Container">
+            <span class="dot disconnected-Dot"></span>
+          </div>
+          <div class="centering-Container">
+            <div class="app-Card-Status-Text disconnected-Text">
+              {{ this.manager.checkApiKey("zenodo")==""?"Disconnected":"Connected" }}
+            </div>
+          </div>
+        </div>
+        <div class="centering-Container center">
+          <span
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Convallis aenean et tortor at risus viverra adipiscing at. Est velit
+            egestas dui id ornare arcu odio ut sem.</span
+          >
+        </div>
+        <div class="centering-Container bottom">
+          <el-button type="text" class="button" @click="updateApi"
+            >{{ this.manager.checkApiKey("zenodo")==""?"Connect":"Disconnect" }}</el-button
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="app-Card">
+      <div class="image-Container">
+        <img
+          src="https://about.zenodo.org/static/img/logos/zenodo-black-2500.png"
+          class="image"
+        />
+      </div>
+      <div class="app-Card-Content">
+        <div class="app-Card-Status">
+          <div class="centering-Container">
+            <span class="dot disconnected-Dot"></span>
+          </div>
+          <div class="centering-Container">
+            <div class="app-Card-Status-Text disconnected-Text">
+              {{ this.manager.checkApiKey("zenodo")==""?"Disconnected":"Connected" }}
+            </div>
+          </div>
+        </div>
+        <div class="centering-Container center">
+          <span
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Convallis aenean et tortor at risus viverra adipiscing at. Est velit
+            egestas dui id ornare arcu odio ut sem.</span
+          >
+        </div>
+        <div class="centering-Container bottom">
+          <el-button type="text" class="button" @click="updateApi"
+            >{{ this.manager.checkApiKey("zenodo")==""?"Connect":"Disconnect" }}</el-button
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="app-Card">
+      <div class="image-Container">
+        <img
+          src="https://about.zenodo.org/static/img/logos/zenodo-black-2500.png"
+          class="image"
+        />
+      </div>
+      <div class="app-Card-Content">
+        <div class="app-Card-Status">
+          <div class="centering-Container">
+            <span class="dot disconnected-Dot"></span>
+          </div>
+          <div class="centering-Container">
+            <div class="app-Card-Status-Text disconnected-Text">
+              {{ this.manager.checkApiKey("zenodo")==""?"Disconnected":"Connected" }}
+            </div>
+          </div>
+        </div>
+        <div class="centering-Container center">
+          <span
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Convallis aenean et tortor at risus viverra adipiscing at. Est velit
+            egestas dui id ornare arcu odio ut sem.</span
+          >
+        </div>
+        <div class="centering-Container bottom">
+          <el-button type="text" class="button" @click="updateApi"
+            >{{ this.manager.checkApiKey("zenodo")==""?"Connect":"Disconnect" }}</el-button
+          >
+        </div>
+      </div>
+    </div>
+
+    <div class="app-Card">
+      <div class="image-Container">
+        <img
+          src="https://about.zenodo.org/static/img/logos/zenodo-black-2500.png"
+          class="image"
+        />
+      </div>
+      <div class="app-Card-Content">
+        <div class="app-Card-Status">
+          <div class="centering-Container">
+            <span class="dot disconnected-Dot"></span>
+          </div>
+          <div class="centering-Container">
+            <div class="app-Card-Status-Text disconnected-Text">
+              {{ this.manager.checkApiKey("zenodo")==""?"Disconnected":"Connected" }}
+            </div>
+          </div>
+        </div>
+        <div class="centering-Container center">
+          <span
+            >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Convallis aenean et tortor at risus viverra adipiscing at. Est velit
+            egestas dui id ornare arcu odio ut sem.</span
+          >
+        </div>
+        <div class="centering-Container bottom">
+          <el-button type="text" class="button" @click="updateApi($event)"
+            >{{ this.manager.checkApiKey("zenodo")==""?"Connect":"Disconnect" }}</el-button
+          >
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { useManage } from "../../store/manage";
+export default {
+  name: "ManageAccount",
+  setup() {
+    const manager = useManage();
+    return { manager };
+  },
+  data() {
+    return {};
+  },
+  methods:{
+    // updateApi(e){
+    //   if(e.target.innerHTML == "Connect"){
+
+    //   } else if(e.target.innerHTML == "Disconnect"){
+
+    //   }
+    // }
+  }
+};
+</script>
+
+<style scoped>
+.page-Container {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 5vw;
+  padding-top: 5vh;
+  padding-bottom: 5vh;
+  overflow: scroll;
+  padding-left: 12vw;
+  padding-right: 12vw;
+  box-sizing: border-box;
+}
+
+.app-Card {
+  display: flex;
+  height: 20vh;
+  width: 100%;
+  padding: 0px;
+  padding-top: 1vh;
+  border-radius: 0.25em;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  box-sizing: border-box;
+}
+
+.image-Container {
+  height: 100%;
+  width: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.app-Card-Content {
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  gap: 1vh;
+  box-sizing: border-box;
+}
+
+.app-Card::after {
+  border-radius: 5px;
+  opacity: 0;
+  transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+}
+
+.app-Card:hover {
+  transform: scale(1.1, 1.1);
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.3);
+}
+
+.app-Card:hover::after {
+  opacity: 1;
+}
+.dot {
+  height: 1.5vmin;
+  width: 1.5vmin;
+  border-radius: 50%;
+  display: inline-block;
+}
+
+.app-Card-Status-Text {
+  font-size: 1.5vmin;
+}
+
+.disconnected-Text {
+  color: grey;
+}
+
+.disconnected-Dot {
+  background-color: lightgray;
+}
+
+.app-Card-Status {
+  height: 10%;
+  display: flex;
+  gap: 0.5vw;
+}
+
+.centering-Container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.center {
+  height: 60%;
+  padding-top: 0.5vh;
+  padding-bottom: 0.5vh;
+  padding-right: 0.8vw;
+  box-sizing: border-box;
+  align-items: flex-start;
+  justify-content: flex-start;
+  overflow: auto;
+  font-size: 1.8vmin;
+  color: rgb(99, 99, 99);
+}
+::-webkit-scrollbar {
+  display: none;
+}
+
+.bottom {
+  height: 30%;
+  justify-content: flex-end;
+  padding-right: 2.2vw;
+  box-sizing: border-box;
+}
+</style>
