15a5a183dc6315e84f4f7f9b21dbef84f2d81326
Sanjay Soundarajan <sanjay.16@live.com>
2021-12-22 10:33:52 -0800

    Sorted the licenses in order

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,7 +3,25 @@
 </template>
 
 <script>
+// import licensesJSON from "../assets/supplementalFiles/licenses.json";
+
 export default {
   name: "HomePage",
+  data() {
+    return {
+      // licenseOptions: licensesJSON.licenses,
+      // sortedLicenses: [],
+    };
+  },
+  methods: {
+    // SortArray(x, y) {
+    //   return x.name.localeCompare(y.name);
+    // },
+  },
+  mounted() {
+    // sort licenses alphabetically
+    // this.sortedLicenses = this.licenseOptions.sort(this.SortArray);
+    // console.log(this.sortedLicenses);
+  },
 };
 </script>
