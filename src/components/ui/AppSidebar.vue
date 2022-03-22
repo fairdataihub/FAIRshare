48b2114bdcf97fd779a5f984d25e082bd726ba13
Sanjay Soundarajan <ssoundarajan@calmi2.org>
2022-03-22 12:41:55 -0700

    Add org logo to sidebar

--- src/components/ui/AppSidebar.vue
+++ src/components/ui/AppSidebar.vue
@@ -65,6 +65,9 @@
             <el-icon><postcard-icon /></el-icon>
             <span v-show="sideBarOpen"> About </span>
           </router-link>
+          <div class="flex flex-col items-center justify-center pt-6">
+            <img src="../../assets/brand/fair-data-innovations-hub-logo.svg" class="w-[150px]" />
+          </div>
           <div class="absolute bottom-0 right-3 hidden">
             <span class="text-xs text-gray-400">{{ environment }}</span>
           </div>
