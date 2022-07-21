c49fbe1c42a1125626b93f720b7ce9ae1e48294c
Sanjay Soundarajan <sanjay.16@live.com>
2022-07-21 13:55:48 -0700

    wip: 🚧 github rework

--- src/views/github/GithubChooseUpload.vue
+++ src/views/github/GithubChooseUpload.vue
@@ -24,9 +24,9 @@
         </div>
       </div>
 
-      <div v-if="addAdditionalFiles !== 'None'" class="flex h-full w-full flex-col items-center">
+      <div v-if="addAdditionalFiles !== 'None'" class="flex w-full flex-col">
         <transition name="fade" mode="out-in" appear>
-          <div v-if="addAdditionalFiles === 'Yes'" class="flex h-full w-full flex-col items-center">
+          <div v-if="addAdditionalFiles === 'Yes'" class="flex h-full w-full flex-col">
             <line-divider class="w-full"></line-divider>
 
             <div class="w-full">
@@ -45,34 +45,129 @@
           </div>
         </transition>
 
-        <div
-          v-if="additionalFilesLocation === 'local'"
-          class="flex h-full w-full flex-col items-center"
-        >
+        <div v-if="additionalFilesLocation === 'local'" class="w-full">
           <el-upload
-            class="upload-demo"
+            class="w-full pt-4 pb-6"
             drag
             multiple
             :auto-upload="false"
             show-file-list
-            ref="fileUploadThing"
+            :on-change="handleAdditionalFilesChange"
+            :on-remove="handleAdditionalFilesChange"
           >
-            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
-            <div class="el-upload__text">Drop file here or <em>click to select</em></div>
-            <template #tip>
-              <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
-            </template>
+            <div class="flex w-full items-center justify-center space-x-4">
+              <Vue3Lottie
+                animationLink="https://assets3.lottiefiles.com/packages/lf20_GxMZME.json"
+                :width="120"
+                :height="120"
+              />
+              <div class="el-upload__text">
+                Drag and drop files here or <em>click to select files</em>
+              </div>
+            </div>
+
+            <!-- Come back here to expand on how the the preview works
+            https://github.com/element-plus/element-plus/blob/dev/packages/components/upload/src/upload-list.vue
+            https://www.npmjs.com/package/byte-size -->
+            <!-- <template #file="{ file }">
+              <div class="el-upload__tip">{{ file }}</div>
+            </template> -->
           </el-upload>
         </div>
-
-        <pre>{{ this.$refs.fileUploadThing.files }}</pre>
 
         <div
           v-if="additionalFilesLocation === 'github'"
           class="flex h-full w-full flex-col items-center"
         >
-          github selector
+          <div class="w-full pt-4 pb-6">
+            <!-- <el-select-v2
+              v-model="selectedRelease"
+              :options="allReleases"
+              placeholder="Please select"
+              size="large"
+              class="w-full"
+            >
+              <template #default="{ item }">
+                <div class="flex flex-col">
+                  <span style="margin-right: 8px">{{ item.label }}</span>
+
+                  <span style="color: var(--el-text-color-secondary); font-size: 13px">
+                    {{ item.value }}
+                  </span>
+                </div>
+              </template>
+            </el-select-v2> -->
+
+            <div class="mb-4">
+              <p class="mb-2">Select the GitHub release you want to use for your Zenodo upload:</p>
+
+              <el-select v-model="selectedRelease" placeholder="Select" class="w-full" size="large">
+                <el-option
+                  v-for="item in allReleases"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                  class="!h-auto transition-all"
+                >
+                  <div class="flex flex-col py-2 leading-normal">
+                    <div class="flex justify-between">
+                      <p class="text-sm font-medium">
+                        {{ item.label }}
+                      </p>
+                      <span class="text-xs font-medium">
+                        {{ publishedDate(item.og.published_at) }}
+                      </span>
+                    </div>
+                    <div class="mt-1 flex flex-row">
+                      <el-tag class="mr-2" type="warning" size="small" v-if="item.og.prerelease">
+                        Pre-release
+                      </el-tag>
+                      <el-tag class="mr-2" size="small"> {{ item.og.tag_name }} </el-tag>
+                    </div>
+                  </div>
+                </el-option>
+              </el-select>
+            </div>
+
+            <div v-if="selectedRelease != ''">
+              <p class="mb-2">
+                Select the files you want to add from your release to the Zenodo dataset
+              </p>
+
+              <el-select
+                v-model="addedReleaseAssets"
+                multiple
+                placeholder="Select"
+                style="width: 240px"
+              >
+                <el-option
+                  v-for="item in releaseAssets"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value"
+                />
+              </el-select>
+            </div>
+          </div>
         </div>
+
+        <transition name="fade" mode="out-in" appear>
+          <div class="flex w-full flex-row justify-center space-x-4 py-2">
+            <router-link
+              :to="`/datasets/${this.$route.params.datasetID}/${this.$route.params.workflowID}/zenodo/metadata`"
+              class=""
+            >
+              <button class="primary-plain-button">
+                <el-icon><d-arrow-left /></el-icon> Back
+              </button>
+            </router-link>
+
+            <button class="primary-button" @click="showSummary" v-if="fileList.length > 0">
+              Continue
+              <el-icon> <d-arrow-right /> </el-icon>
+            </button>
+          </div>
+        </transition>
       </div>
     </div>
     <app-docs-link url="curate-and-share/connect-github-zenodo" position="bottom-4" />
@@ -82,6 +177,9 @@
 <script>
 import { useDatasetsStore } from "@/store/datasets";
 import { useTokenStore } from "@/store/access.js";
+
+import axios from "axios";
+import dayjs from "dayjs";
 
 export default {
   name: "GithubChooseUpload",
@@ -98,8 +196,33 @@ export default {
       selectedRepo: "",
       fileList: [],
       addAdditionalFiles: "Yes",
-      additionalFilesLocation: "local",
+      additionalFilesLocation: "github",
+      allReleases: [],
+      selectedRelease: "",
+      addedReleaseAssets: [],
     };
+  },
+
+  computed: {
+    releaseAssets() {
+      if (this.selectedRelease === "") {
+        return [];
+      }
+
+      console.log(this.selectedRelease);
+
+      const assets = this.allReleases.find((release) => release.value === this.selectedRelease).og
+        .assets;
+
+      console.log("releaseAssets", assets);
+
+      return assets.map((asset) => {
+        return {
+          label: asset.name,
+          value: asset.id,
+        };
+      });
+    },
   },
 
   methods: {
@@ -108,9 +231,49 @@ export default {
       window.ipcRenderer.send("open-link-in-browser", url);
     },
 
-    async cancelUpload(files) {
-      console.log(files);
-      return false;
+    publishedDate(date) {
+      return dayjs(date).format("MMM DD, YYYY");
+    },
+
+    async handleAdditionalFilesChange(uploadFile, uploadFiles) {
+      console.log(uploadFile, uploadFiles);
+      this.fileList = uploadFiles;
+    },
+
+    async getReleases() {
+      const splitRepoNameOwner = this.selectedRepo.split("/");
+
+      const releaseList = await axios
+        .get(`${this.$server_url}/github/repo/releases`, {
+          params: {
+            access_token: this.GithubAccessToken,
+            owner: splitRepoNameOwner[0],
+            repo: splitRepoNameOwner[1],
+          },
+        })
+        .then((response) => {
+          return response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+          return "ERROR";
+        });
+
+      console.log(releaseList);
+
+      if (releaseList !== "ERROR") {
+        if (releaseList.length > 0) {
+          this.allReleases = [];
+
+          for (let release of releaseList) {
+            this.allReleases.push({
+              label: release.name,
+              value: release.id,
+              og: release,
+            });
+          }
+        }
+      }
     },
 
     async showSummary() {
@@ -141,6 +304,8 @@ export default {
 
     const tokenObject = await this.tokens.getToken("github");
     this.GithubAccessToken = tokenObject.token;
+
+    this.getReleases();
 
     // }
 
