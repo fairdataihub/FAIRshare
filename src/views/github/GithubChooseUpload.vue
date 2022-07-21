ee0dfb09f41bd49f089c3a84e489377cad994a0a
Sanjay Soundarajan <sanjay.16@live.com>
2022-07-20 17:23:24 -0700

    wip: 🚧 github rework

--- src/views/github/GithubChooseUpload.vue
+++ src/views/github/GithubChooseUpload.vue
@@ -8,6 +8,72 @@
       </p>
 
       <line-divider />
+
+      <div class="w-full">
+        <span class="mb-2 w-full">
+          Would you like to add additional files to your upload? We will handle all your source code
+          from the GitHub repository.
+        </span>
+
+        <div class="py-1">
+          <el-radio v-model="addAdditionalFiles" label="Yes" size="large"> Yes </el-radio>
+          <el-radio v-model="addAdditionalFiles" label="No" size="large"> No </el-radio>
+          <el-radio v-model="addAdditionalFiles" label="None" size="large" border class="!hidden">
+            None
+          </el-radio>
+        </div>
+      </div>
+
+      <div v-if="addAdditionalFiles !== 'None'" class="flex h-full w-full flex-col items-center">
+        <transition name="fade" mode="out-in" appear>
+          <div v-if="addAdditionalFiles === 'Yes'" class="flex h-full w-full flex-col items-center">
+            <line-divider class="w-full"></line-divider>
+
+            <div class="w-full">
+              <span class="mb-4 w-full">
+                Are these files stored on your computer or do you want the files to be downloaded
+                from a GitHub release?
+              </span>
+
+              <div class="mt-3 flex flex-row justify-start py-1">
+                <el-radio-group v-model="additionalFilesLocation" size="large">
+                  <el-radio-button label="local"> On my computer </el-radio-button>
+                  <el-radio-button label="github"> Pick a GitHub release </el-radio-button>
+                </el-radio-group>
+              </div>
+            </div>
+          </div>
+        </transition>
+
+        <div
+          v-if="additionalFilesLocation === 'local'"
+          class="flex h-full w-full flex-col items-center"
+        >
+          <el-upload
+            class="upload-demo"
+            drag
+            multiple
+            :auto-upload="false"
+            show-file-list
+            ref="fileUploadThing"
+          >
+            <el-icon class="el-icon--upload"><upload-filled /></el-icon>
+            <div class="el-upload__text">Drop file here or <em>click to select</em></div>
+            <template #tip>
+              <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
+            </template>
+          </el-upload>
+        </div>
+
+        <pre>{{ this.$refs.fileUploadThing.files }}</pre>
+
+        <div
+          v-if="additionalFilesLocation === 'github'"
+          class="flex h-full w-full flex-col items-center"
+        >
+          github selector
+        </div>
+      </div>
     </div>
     <app-docs-link url="curate-and-share/connect-github-zenodo" position="bottom-4" />
   </div>
@@ -16,10 +82,6 @@
 <script>
 import { useDatasetsStore } from "@/store/datasets";
 import { useTokenStore } from "@/store/access.js";
-
-import { ElLoading } from "element-plus";
-
-import rippleLottieJSON from "@/assets/lotties/rippleLottie.json";
 
 export default {
   name: "GithubChooseUpload",
@@ -33,33 +95,34 @@ export default {
       workflowID: this.$route.params.workflowID,
       workflow: {},
       GithubAccessToken: "",
-      validZenodoHookTokenFound: false,
-      errorMessage: "",
-      zenodoAccessToken: "",
       selectedRepo: "",
-      currentBranch: "",
-      rippleLottieJSON,
-      finishedLoading: false,
-      interval: null,
+      fileList: [],
+      addAdditionalFiles: "Yes",
+      additionalFilesLocation: "local",
     };
   },
 
   methods: {
-    createLoading() {
-      const loading = ElLoading.service({
-        lock: true,
-        text: "Reading GitHub repository...",
-        background: "rgba(255, 255, 255, 0.97)",
-      });
-      return loading;
-    },
-
     openWebsite() {
       const url = `${process.env.VUE_APP_ZENODO_URL}/account/settings/github`;
       window.ipcRenderer.send("open-link-in-browser", url);
     },
 
+    async cancelUpload(files) {
+      console.log(files);
+      return false;
+    },
+
     async showSummary() {
+      if (this.addAdditionalFiles === "Yes") {
+        this.workflow.addAdditionalFiles = true;
+      } else {
+        this.workflow.addAdditionalFiles = false;
+      }
+
+      await this.datasetStore.updateCurrentDataset(this.dataset);
+      await this.datasetStore.syncDatasets();
+
       const routerPath = `/datasets/${this.datasetID}/${this.workflowID}/github/summary`;
 
       this.$router.push({ path: routerPath });
