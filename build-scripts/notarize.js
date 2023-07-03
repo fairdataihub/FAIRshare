91175be1f1853fc1ee5bcdfe1c4471999a27b714
Sanjay Soundarajan <sanjay.16@live.com>
2023-07-03 16:06:28 -0700

    chore(deps): 🔗 update deprecated dependencies

--- build-scripts/notarize.js
+++ build-scripts/notarize.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const electron_notarize = require("@electron-notarize");
+const electron_notarize = require("@electron/notarize");
 
 require("dotenv").config();
 
